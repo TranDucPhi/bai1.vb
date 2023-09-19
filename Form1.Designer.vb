705e1f8119c6be93fb5c1eb33799a426e8981e2d
MAY 06 <MAY 06@LAB2-35>
2023-09-19 09:00:29 +0700

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,110 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txttenfile = New System.Windows.Forms.TextBox()
+        Me.btndoc = New System.Windows.Forms.Button()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtnoidung = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(163, 235)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(116, 25)
+        Me.Label3.TabIndex = 0
+        Me.Label3.Text = "Nội Dung:"
+        '
+        'txttenfile
+        '
+        Me.txttenfile.Location = New System.Drawing.Point(285, 170)
+        Me.txttenfile.Name = "txttenfile"
+        Me.txttenfile.Size = New System.Drawing.Size(223, 20)
+        Me.txttenfile.TabIndex = 1
+        '
+        'btndoc
+        '
+        Me.btndoc.Location = New System.Drawing.Point(582, 170)
+        Me.btndoc.Name = "btndoc"
+        Me.btndoc.Size = New System.Drawing.Size(90, 30)
+        Me.btndoc.TabIndex = 2
+        Me.btndoc.Text = "Đọc"
+        Me.btndoc.UseVisualStyleBackColor = True
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(163, 170)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(105, 25)
+        Me.Label4.TabIndex = 0
+        Me.Label4.Text = "Tên File:"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(280, 76)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(99, 25)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Đọc File"
+        '
+        'txtnoidung
+        '
+        Me.txtnoidung.Location = New System.Drawing.Point(285, 241)
+        Me.txtnoidung.Multiline = True
+        Me.txtnoidung.Name = "txtnoidung"
+        Me.txtnoidung.Size = New System.Drawing.Size(223, 144)
+        Me.txtnoidung.TabIndex = 1
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.btndoc)
+        Me.Controls.Add(Me.txtnoidung)
+        Me.Controls.Add(Me.txttenfile)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txttenfile As TextBox
+    Friend WithEvents btndoc As Button
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtnoidung As TextBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,14 @@
+﻿Public Class Form1
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub btndoc_Click(sender As Object, e As EventArgs) Handles btndoc.Click
+        Dim tenfile As String
+        tenfile = txttenfile.Text
+        Dim sr As System.IO.StreamReader
+        sr = New System.IO.StreamReader("D:\\" & tenfile)
+        txtnoidung.Text = sr.ReadToEnd()
+        sr.Close()
+    End Sub
+End Class
